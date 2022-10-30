44b2e26b6cae8c46cc5713a9c169f88be6bd6b24
Ugo <ugocruise@gmail.com>
2022-10-30 02:39:30 +0100

    first commit colorless

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import domtoimage from "dom-to-image";
 
 function App() {
+  const [quotes, setQuotes] = useState("");
+  const fetchQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let quoteRandomizer = Math.floor(Math.random() * data.length);
+        setQuotes(data[quoteRandomizer]);
+      });
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const downloadRef = useRef();
+
+  const downloadImage = () => {
+    const targetElement = downloadRef.current;
+    domtoimage.toJpeg(targetElement, { quality: 0.95 }).then((dataUrl) => {
+      let link = document.createElement("a");
+      link.download = "my-image-name.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+           <div className="header"></div>
+      <div className="App" ref={downloadRef}>
+        <div className="quote-body">
+          <div className="text">
+            <blockquote>
+            
+              <q>{quotes.text}</q>
+            </blockquote>
+          </div>
+          <div className="author">
+                       {quotes.author}
+          </div>
+          <div className="buttons">
+            <div className="spaced">
+              {" "}
+              <a
+                // eslint-disable-next-line no-template-curly-in-string
+                href="https://twitter.com/intent/tweet?text=${quotes.text}"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div>
+              {" "}
+              <button onClick={fetchQuote}> New Quote </button>
+              <button onClick={downloadImage}> Download</button>
+            </div>
+          </div>{" "}
+        </div>
+      </div>
     </div>
   );
 }
