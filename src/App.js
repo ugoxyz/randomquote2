928c4ea420a7f03b18ae70938f97f52a469b6878
Ugo <ugocruise@gmail.com>
2022-11-01 01:03:39 +0100

    Added randomizer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import domtoimage from "dom-to-image";
 
 function App() {
   const [quotes, setQuotes] = useState("");
+
   const fetchQuote = () => {
     fetch("https://type.fit/api/quotes")
       .then((res) => res.json())
@@ -17,13 +18,35 @@ function App() {
     fetchQuote();
   }, []);
 
+  // const [mainColor,setColor] = useState("#defdef");
+  // let randomColor = Math.floor(Math.random() * MY_COLORS.length);
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857",
+  ];
+
+  let randomColor = Math.floor(Math.random() * colors.length);
+  let myColor = colors[randomColor];
+
+  document.body.style.backgroundColor = myColor;
+
   const downloadRef = useRef();
 
   const downloadImage = () => {
     const targetElement = downloadRef.current;
     domtoimage.toJpeg(targetElement, { quality: 0.95 }).then((dataUrl) => {
       let link = document.createElement("a");
-      link.download = "my-image-name.jpeg";
+      link.download = "my-quote.jpeg";
       link.href = dataUrl;
       link.click();
     });
@@ -31,38 +54,41 @@ function App() {
 
   return (
     <div>
-           <div className="header"></div>
-      <div className="App" ref={downloadRef}>
-        <div className="quote-body">
-          <div className="text">
-            <blockquote>
-            
-              <q>{quotes.text}</q>
-            </blockquote>
-          </div>
-          <div className="author">
-                       {quotes.author}
-          </div>
-          <div className="buttons">
-            <div className="spaced">
-              {" "}
-              <a
-                // eslint-disable-next-line no-template-curly-in-string
-                href="https://twitter.com/intent/tweet?text=${quotes.text}"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {" "}
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-              </a>
+      <div className="container">
+        <div className="App" ref={downloadRef}>
+          <div>
+            <div className="quote-body">
+              <div className="text">
+                <blockquote style={{ color: myColor }}>
+                  <q> {quotes.text}</q>
+                </blockquote>
+              </div>
+              <div className="author" style={{ color: myColor }}>{quotes.author}</div>
             </div>
-            <div>
-              {" "}
-              <button onClick={fetchQuote}> New Quote </button>
-              <button onClick={downloadImage}> Download</button>
-            </div>
-          </div>{" "}
+          </div>
         </div>
+        <div className="button-div">
+          <div className="spaced">
+            {" "}
+            <a
+              // eslint-disable-next-line no-template-curly-in-string
+              href="https://twitter.com/intent/tweet?text=${quotes.text}"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div>
+            {" "}
+            <button className="my-button" onClick={fetchQuote} style={{ backgroundColor: myColor, cursor: "pointer"}}>
+              {" "}
+              New Quote{" "}
+            </button >
+            <button className="my-button" onClick={downloadImage}> Download</button>
+          </div>
+        </div>{" "}
       </div>
     </div>
   );
---
+++ src/Colorizer.js
@@ -0,0 +1,47 @@
+const MY_COLORS = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+];
+
+// export default function changeMe() {
+//     let oneColor = colors.length;
+//     let randomColor = Math.floor(Math.random() * oneColor);
+//     let colorSeed = colors[randomColor];
+
+//  //   document.body.style.background = colorSeed;
+
+//     document.getElementById('new-quote').addEventListener('click', 
+    
+//     function colorListener() {
+//         document.getElementById('new-quote').style.backgroundColor = colorSeed;
+//         document.body.style.backgroundColor = colorSeed;
+//         document.getElementById("quote-text").style.color = colorSeed;
+//         document.getElementById("author").style.color = colorSeed;
+//         document.getElementById("tweet-quote").style.color = colorSeed;
+        
+      
+//         document.querySelector("html body").animate({
+//             backgroundColor: colorSeed,
+//             color: colorSeed
+//         }, 500);
+//         document.querySelector(".button").animate({
+//             backgroundColor: colorSeed,
+//             color: colorSeed
+//         }, 500);
+//         document.querySelector(".quote-text").animate({
+//             color: colorSeed
+//         },500);
+//     });
+// }
+
+export default MY_COLORS;
