580b3cbfd54253d1f7975d3463b94104e0381888
Ugo <ugocruise@gmail.com>
2023-04-28 01:23:35 +0100

    New updates with Dokkodo quotes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import domtoimage from "dom-to-image";
+import "./quotes.js";
+import AllQuotes from "./quotes.js";
 
 function App() {
   const [quotes, setQuotes] = useState("");
 
   const fetchQuote = () => {
-    fetch("https://type.fit/api/quotes")
-      .then((res) => res.json())
-      .then((data) => {
-        let quoteRandomizer = Math.floor(Math.random() * data.length);
-        setQuotes(data[quoteRandomizer]);
-      });
+    let quoteRandomizer = Math.floor(Math.random() * AllQuotes.length);
+    setQuotes(AllQuotes[quoteRandomizer]);
   };
 
-  useEffect(() => {
-    fetchQuote();
-  }, []);
+  // const fetchQuote = () => {
+  //   fetch("https://type.fit/api/quotes")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       let quoteRandomizer = Math.floor(Math.random() * data.length);
+  //       setQuotes(data[quoteRandomizer]);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   fetchQuote();
+  // }, []);
 
   // const [mainColor,setColor] = useState("#defdef");
   // let randomColor = Math.floor(Math.random() * MY_COLORS.length);
@@ -63,12 +70,32 @@ function App() {
                   <q> {quotes.text}</q>
                 </blockquote>
               </div>
-              <div className="author" style={{ color: myColor }}>{quotes.author}</div>
+              <div className="author-box">
+                {" "}
+                <div>
+                  {" "}
+                  <img
+                    className="avi"
+                    alt="xcc"
+                    src={quotes.avi}>
+
+                  </img>
+                </div>
+                <div>
+                     <div className="author" style={{ color: myColor }}>
+                  {quotes.author}
+                </div>
+                <div className="author-title" style={{ color: myColor }}>
+                  {quotes.title}
+                </div>
+                </div>
+             
+              </div>
             </div>
           </div>
         </div>
         <div className="button-div">
-          <div className="spaced">
+          {/* <div className="spaced">
             {" "}
             <a
               // eslint-disable-next-line no-template-curly-in-string
@@ -79,14 +106,21 @@ function App() {
               {" "}
               <i class="fa fa-twitter" aria-hidden="true"></i>
             </a>
-          </div>
+          </div> */}
           <div>
             {" "}
-            <button className="my-button" onClick={fetchQuote} style={{ backgroundColor: myColor, cursor: "pointer"}}>
+            <button
+              className="my-button"
+              onClick={fetchQuote}
+              style={{ backgroundColor: myColor, cursor: "pointer" }}
+            >
               {" "}
               New Quote{" "}
-            </button >
-            <button className="my-button" onClick={downloadImage}> Download</button>
+            </button>
+            <button className="my-button" onClick={downloadImage}>
+              {" "}
+              Download
+            </button>
           </div>
         </div>{" "}
       </div>
